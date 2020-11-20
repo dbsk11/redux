c51769eec047b1d026a356279fafacec25b69bef
Jessica Kim <jessikim807@gmail.com>
2020-11-20 09:33:49 -0500

    installed redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+// REDUX
+import { createStore } from 'redux'
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
